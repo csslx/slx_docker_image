6ae357c0548335d8d3218462f8419b908dafc1d1
csslx <chensen151222@gmail.com>
2026-03-11 03:37:20 +0000

    update portainer

--- Beta/portainer/start.sh
+++ Beta/portainer/start.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-mkdir /opt/portainer
-wget 
+mkdir -p /slxapp/portainer
+cd /slxapp/portainer
+curl -O https://raw.githubusercontent.com/csslx/slx_docker_image/refs/heads/main/Beta/portainer/docker-compose.yml
+docker-compose up -d
